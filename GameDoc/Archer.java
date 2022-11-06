1d7e713df80c0de84d9873df1f302cde447da38b
Murat Can Özhan <84957193+muratcanzhan@users.noreply.github.com>
2022-11-06 12:58:57 +0300

    Add files via upload

---
+++ GameDoc/Archer.java
@@ -0,0 +1,7 @@
+public class Archer extends GameCharacter {
+    public Archer() {
+        super("Archer", 2, 7, 18, 20);
+
+    }
+
+}
---
+++ GameDoc/Exit.java
@@ -0,0 +1,11 @@
+public class Exit extends NormalLocation {
+    public Exit(Player player) {
+        super(0, player, "Log out");
+    }
+
+    @Override
+    public boolean onLocation() {
+        System.out.println("Checked Out");
+        return true;
+    }
+}
